0b084cf414d770085724ef4c14682667fcdd7737
yuzhongyangwang <33951335+yuzhongyangwang@users.noreply.github.com>
2019-05-18 15:29:38 +0800

    finished GameTest

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,10 +1,51 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.generator.RandomIntGenerator;
+import tw.core.model.GuessResult;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
 
 
 public class GameTest {
+    @Test
+    public void should_return_guessed_result_1() throws OutOfRangeAnswerException{
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Game game = new Game(answerGenerator);
+        Answer answer = new Answer();
+        answer.setNumList(new ArrayList<String>(){{
+            add("1");
+            add("3");
+            add("5");
+            add("7");
+        }});
+        GuessResult exceptedGuessResult = game.guess(answer);
+        assertThat(exceptedGuessResult.getResult().contains("A"),is(true));
+    }
 
+    @Test
+    public void should_return_guessed_result_2() throws OutOfRangeAnswerException{
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Game game = new Game(answerGenerator);
+        Answer answer = new Answer();
+        answer.setNumList(new ArrayList<String>(){{
+            add("1");
+            add("3");
+            add("5");
+            add("7");
+        }});
+        GuessResult exceptedGuessResult = game.guess(answer);
+        assertThat(exceptedGuessResult.getResult().length(),is(4));
+    }
 }
